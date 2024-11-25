af319e0937e6d590d4c19832b494d4b0e2a78ce3
Christopher Begnaud <christopherbegnaud@gmail.com>
2024-11-24 18:37:23 -0700

    Changed folder names.

--- class26-homework/api-cocktails-carousel/js/main.js
+++ class26-homework/api-cocktails-carousel/js/main.js
@@ -85,24 +85,3 @@ function getIngredientsParagraph(apiObject) {
   console.log(ingredients);
   return ingredients.join(", ");
 }
-
-
-
-// send request with a space encoded with %20
-// function cocktailInfo() {
-//   let cocktail = document.querySelector('input').value;
-//   console.log(cocktail);
-//   cocktail = cocktail.split(' ').join('%20');
-//   console.log(cocktail);
-//   fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + cocktail)
-//   .then(res => res.json())
-//   .then(data => {
-//     console.log(data);
-//     console.log(data.drinks[0].strDrink);
-//     document.querySelector('h2').innerText = data.drinks[0].strDrink;
-//     document.querySelector('p').innerText = data.drinks[0].strInstructions;
-//   })
-//   .catch(err => {
-//     console.log(`error ${err}`)
-//   });
-// }
